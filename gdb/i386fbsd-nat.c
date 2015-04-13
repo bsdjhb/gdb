ea8481544b418a32664848c50948c2963befdb94
John Baldwin <jhb@FreeBSD.org>
2015-04-13 18:27:28 -0400

    Import simple port patches.

--- gdb/i386fbsd-nat.c
+++ gdb/i386fbsd-nat.c
@@ -175,7 +175,6 @@ _initialize_i386fbsd_nat (void)
   t->to_read_description = i386fbsd_read_description;
 #endif
 
-  t->to_resume = i386fbsd_resume;
   t->to_pid_to_exec_file = fbsd_pid_to_exec_file;
   t->to_find_memory_regions = fbsd_find_memory_regions;
   add_target (t);
