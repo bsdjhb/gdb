105f4c56bf149a5d5470a799b29789b3da94c754
John Baldwin <jhb@FreeBSD.org>
2015-06-03 06:20:31 -0400

    First cut at porting sparc64 target (including cross-debugging).

--- gdb/kgdb/trgt_sparc64.c
+++ gdb/kgdb/trgt_sparc64.c
@@ -28,62 +28,149 @@
 __FBSDID("$FreeBSD: head/gnu/usr.bin/gdb/kgdb/trgt_sparc64.c 246893 2013-02-17 02:15:19Z marcel $");
 
 #include <sys/types.h>
+#ifdef __sparc64__
 #include <machine/asm.h>
 #include <machine/pcb.h>
 #include <machine/frame.h>
-#include <err.h>
-#include <kvm.h>
+#endif
 #include <string.h>
 
 #include <defs.h>
+#include "osabi.h"
 #include <target.h>
-#include <gdbthread.h>
-#include <inferior.h>
-#include <regcache.h>
 #include <frame-unwind.h>
+#include "solib.h"
+#include "trad-frame.h"
 #include <sparc-tdep.h>
 #include <sparc64-tdep.h>
 
 #include "kgdb.h"
 
-CORE_ADDR
-kgdb_trgt_core_pcb(u_int cpuid)
+#ifdef __sparc64__
+static CORE_ADDR
+sparc64fbsd_cpu_pcb_addr(u_int cpuid)
 {
 	return (kgdb_trgt_stop_pcb(cpuid, sizeof(struct pcb)));
 }
 
-void
-kgdb_trgt_fetch_registers(int regno __unused)
+static void
+sparc64fbsd_supply_pcb(struct regcache *regcache, CORE_ADDR pcb_addr)
 {
-	struct kthr *kt;
 	struct pcb pcb;
 
-	kt = kgdb_thr_lookup_tid(ptid_get_pid(inferior_ptid));
-	if (kt == NULL)
-		return;
-	if (kvm_read(kvm, kt->pcb, &pcb, sizeof(pcb)) != sizeof(pcb)) {
-		warnx("kvm_read: %s", kvm_geterr(kvm));
+	if (target_read_memory(pcb_addr, &pcb, sizeof(pcb)) != 0)
 		memset(&pcb, 0, sizeof(pcb));
-	}
 
-	supply_register(SPARC_SP_REGNUM, (char *)&pcb.pcb_sp);
-	sparc_supply_rwindow(current_regcache, pcb.pcb_sp, -1);
-	supply_register(SPARC64_PC_REGNUM, (char *)&pcb.pcb_pc);
+	regcache_raw_supply(regcache, SPARC_SP_REGNUM, (char *)&pcb.pcb_sp);
+	sparc_supply_rwindow(regcache, pcb.pcb_sp, -1);
+	regcache_raw_supply(regcache, SPARC64_PC_REGNUM, (char *)&pcb.pcb_pc);
 	pcb.pcb_pc += 4;
-	supply_register(SPARC64_NPC_REGNUM, (char *)&pcb.pcb_pc);
+	regcache_raw_supply(regcache, SPARC64_NPC_REGNUM, (char *)&pcb.pcb_pc);
 }
+#endif
 
-void
-kgdb_trgt_store_registers(int regno __unused)
+#define	OFF_TF_SP	(14 * 8)
+#define	OFF_TF_TPC	(25 * 8)
+#define	OFF_TF_TNPC	(24 * 8)
+#define	OFF_TF_OUT	(8 * 8)
+#define	TRAPFRAME_SIZE	(32 * 8)
+
+#ifdef __sparc64__
+_Static_assert(sizeof(struct trapframe) == TRAPFRAME_SIZE, "trapframe size");
+_Static_assert(offsetof(struct trapframe, tf_sp) == OFF_TF_SP, "tf_sp offset");
+_Static_assert(offsetof(struct trapframe, tf_tpc) == OFF_TF_TPC,
+	       "tf_tpc offset");
+_Static_assert(offsetof(struct trapframe, tf_tnpc) == OFF_TF_TNPC,
+	       "tf_tnpc offset");
+_Static_assert(offsetof(struct trapframe, tf_out) == OFF_TF_OUT,
+	       "tf_out offset");
+#endif
+
+static struct sparc_frame_cache *
+sparc64fbsd_trapframe_cache (struct frame_info *this_frame, void **this_cache)
 {
-	fprintf_unfiltered(gdb_stderr, "XXX: %s\n", __func__);
+  struct sparc_frame_cache *cache;
+  CORE_ADDR fp, sp, trapframe_addr;
+  int regnum;
+
+  if (*this_cache)
+    return *this_cache;
+
+  cache = sparc_frame_cache (this_frame, this_cache);
+  gdb_assert (cache == *this_cache);
+
+  fp = get_frame_register_unsigned (this_frame, SPARC_FP_REGNUM);
+  trapframe_addr = fp + BIAS - TRAPFRAME_SIZE;
+  sp = get_frame_register_unsigned (this_frame, SPARC_SP_REGNUM);
+
+  cache->saved_regs = trad_frame_alloc_saved_regs (this_frame);
+
+  cache->saved_regs[SPARC_SP_REGNUM].addr = trapframe_addr + OFF_TF_SP;
+#ifdef notyet
+  cache->saved_regs[SPARC64_STATE_REGNUM].addr = trapframe_addr + OFF_TF_TSTATE;
+#endif
+  cache->saved_regs[SPARC64_PC_REGNUM].addr = trapframe_addr + OFF_TF_TPC;
+  cache->saved_regs[SPARC64_NPC_REGNUM].addr = trapframe_addr + OFF_TF_TNPC;
+  for (regnum = SPARC_O0_REGNUM; regnum <= SPARC_O7_REGNUM; regnum++)
+    cache->saved_regs[regnum].addr =
+      trapframe_addr + OFF_TF_OUT + (regnum - SPARC_O0_REGNUM) * 8;
+  for (regnum = SPARC_L0_REGNUM; regnum <= SPARC_I7_REGNUM; regnum++)
+    cache->saved_regs[regnum].addr =
+      sp + BIAS + (regnum - SPARC_L0_REGNUM) * 8;
+
+  return cache;
 }
 
-void
-kgdb_trgt_new_objfile(struct objfile *objfile)
+static void
+sparc64fbsd_trapframe_this_id (struct frame_info *this_frame,
+			       void **this_cache, struct frame_id *this_id)
 {
+  struct sparc_frame_cache *cache =
+    sparc64fbsd_trapframe_cache (this_frame, this_cache);
+
+  (*this_id) = frame_id_build (cache->base, cache->pc);
 }
 
+static struct value *
+sparc64fbsd_trapframe_prev_register (struct frame_info *this_frame,
+				     void **this_cache, int regnum)
+{
+  struct sparc_frame_cache *cache =
+    sparc64fbsd_trapframe_cache (this_frame, this_cache);
+
+  return trad_frame_get_prev_register (this_frame, cache->saved_regs, regnum);
+}
+
+static int
+sparc64fbsd_trapframe_sniffer (const struct frame_unwind *self,
+			       struct frame_info *this_frame,
+			       void **this_cache)
+{
+  CORE_ADDR pc;
+  const char *name;
+
+  pc = get_frame_address_in_block (this_frame);
+  find_pc_partial_function (pc, &name, NULL, NULL);
+  if (name && (strcmp(name, "tl0_intr") == 0
+	       || strcmp(name, "tl0_trap") == 0
+	       || strcmp(name, "tl1_intr") == 0
+	       || strcmp(name, "tl1_trap") == 0))
+    return 1;
+
+  return 0;
+}
+
+static const struct frame_unwind sparc64fbsd_trapframe_unwind =
+{
+  NORMAL_FRAME,
+  default_frame_unwind_stop_reason,
+  sparc64fbsd_trapframe_this_id,
+  sparc64fbsd_trapframe_prev_register,
+  NULL,
+  sparc64fbsd_trapframe_sniffer
+};
+
+#if 0
 struct kgdb_frame_cache {
 	CORE_ADDR	pc;
 	CORE_ADDR	sp;
@@ -203,3 +290,30 @@ kgdb_trgt_trapframe_sniffer(struct frame_info *next_frame)
 	/* printf("%s: %lx =%s\n", __func__, pc, pname); */
 	return (NULL);
 }
+#endif
+
+static void
+sparc64fbsd_kernel_init_abi(struct gdbarch_info info, struct gdbarch *gdbarch)
+{
+
+	sparc64_init_abi(info, gdbarch);
+
+	frame_unwind_prepend_unwinder(gdbarch, &sparc64fbsd_trapframe_unwind);
+
+	set_solib_ops(gdbarch, &kld_so_ops);
+
+#ifdef __sparc64__
+	fbsd_vmcore_set_supply_pcb(gdbarch, sparc64fbsd_supply_pcb);
+	fbsd_vmcore_set_cpu_pcb_addr(gdbarch, sparc64fbsd_cpu_pcb_addr);
+#endif
+}
+
+void _initialize_sparc64_kgdb_tdep(void);
+
+void
+_initialize_sparc64_kgdb_tdep(void)
+{
+	gdbarch_register_osabi (bfd_arch_sparc, bfd_mach_sparc_v9,
+	    GDB_OSABI_FREEBSD_ELF_KERNEL, sparc64fbsd_kernel_init_abi);
+}
+
