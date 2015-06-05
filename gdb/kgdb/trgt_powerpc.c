2fdb7223ac60180b7dac28983549d7b0016d1840
John Baldwin <jhb@FreeBSD.org>
2015-06-05 15:17:21 -0400

    First cut at porting the powerpc targets.
    As with the user targets, both are present in a single file.

--- gdb/kgdb/trgt_powerpc.c
+++ gdb/kgdb/trgt_powerpc.c
@@ -28,174 +28,231 @@
 __FBSDID("$FreeBSD: head/gnu/usr.bin/gdb/kgdb/trgt_powerpc.c 246893 2013-02-17 02:15:19Z marcel $");
 
 #include <sys/types.h>
-#ifdef CROSS_DEBUGGER
-#include <sys/powerpc/include/pcb.h>
-#include <sys/powerpc/include/frame.h>
-#else
+#ifdef __powerpc__
 #include <machine/pcb.h>
 #include <machine/frame.h>
 #endif
-#include <err.h>
-#include <kvm.h>
 #include <string.h>
 
 #include <defs.h>
+#if 0
 #include <target.h>
 #include <gdbthread.h>
 #include <inferior.h>
 #include <regcache.h>
+#endif
 #include <frame-unwind.h>
+#include "osabi.h"
+#include "solib.h"
+#include "symtab.h"
+#include "trad-frame.h"
+
 #include <ppc-tdep.h>
+#include "ppc64-tdep.h"
 
 #include "kgdb.h"
 
-CORE_ADDR
-kgdb_trgt_core_pcb(u_int cpuid)
+#ifdef __powerpc__
+static CORE_ADDR
+ppcfbsd_cpu_pcb_addr(u_int cpuid)
 {
 	return (kgdb_trgt_stop_pcb(cpuid, sizeof(struct pcb)));
 }
 
-void
-kgdb_trgt_fetch_registers(int regno __unused)
+static void
+ppcfbsd_supply_pcb(struct regcache *regcache, CORE_ADDR pcb_addr)
 {
-	struct kthr *kt;
 	struct pcb pcb;
 	struct gdbarch_tdep *tdep;
 	int i;
 
-	tdep = gdbarch_tdep (current_gdbarch);
+	tdep = gdbarch_tdep (target_gdbarch());
 
-	kt = kgdb_thr_lookup_tid(ptid_get_pid(inferior_ptid));
-	if (kt == NULL)
-		return;
-	if (kvm_read(kvm, kt->pcb, &pcb, sizeof(pcb)) != sizeof(pcb)) {
-		warnx("kvm_read: %s", kvm_geterr(kvm));
+	if (target_read_memory(pcb_addr, &pcb, sizeof(pcb)) != 0)
 		memset(&pcb, 0, sizeof(pcb));
-	}
 
 	/*
 	 * r14-r31 are saved in the pcb
 	 */
 	for (i = 14; i <= 31; i++) {
-		supply_register(tdep->ppc_gp0_regnum + i,
+		regcache_raw_supply(regcache, tdep->ppc_gp0_regnum + i,
 		    (char *)&pcb.pcb_context[i]);
 	}
 
 	/* r1 is saved in the sp field */
-	supply_register(tdep->ppc_gp0_regnum + 1, (char *)&pcb.pcb_sp);
+	regcache_raw_supply(regcache, tdep->ppc_gp0_regnum + 1,
+			    (char *)&pcb.pcb_sp);
+	if (tdep->wordsize == 8)
+	  /* r2 is saved in the toc field */
+	  regcache_raw_supply(regcache, tdep->ppc_gp0_regnum + 2,
+			      (char *)&pcb.pcb_toc);
 
-	supply_register(tdep->ppc_lr_regnum, (char *)&pcb.pcb_lr);
-	supply_register(tdep->ppc_cr_regnum, (char *)&pcb.pcb_cr);
+	regcache_raw_supply(regcache, tdep->ppc_lr_regnum, (char *)&pcb.pcb_lr);
+	regcache_raw_supply(regcache, tdep->ppc_cr_regnum, (char *)&pcb.pcb_cr);
 }
+#endif
 
-void
-kgdb_trgt_store_registers(int regno __unused)
+#define	OFF_FIXREG	0
+#define	OFF_LR		32
+#define	OFF_CR		33
+#define	OFF_XER		34
+#define	OFF_CTR		35
+#define	OFF_SRR0	36
+#define	TRAPFRAME_SIZE	42
+
+#ifdef __powerpc__
+_Static_assert(sizeof(struct trapframe) == TRAPFRAME_SIZE * sizeof(register_t),
+	       "trapframe size");
+_Static_assert(offsetof(struct trapframe, fixreg)
+	       == OFF_FIXREG * sizeof(register_t), "fixreg offset");
+_Static_assert(offsetof(struct trapframe, lr) == OFF_LR * sizeof(register_t),
+	       "lr offset");
+_Static_assert(offsetof(struct trapframe, cr) == OFF_CR * sizeof(register_t),
+	       "cr offset");
+_Static_assert(offsetof(struct trapframe, xer) == OFF_XER * sizeof(register_t),
+	       "xer offset");
+_Static_assert(offsetof(struct trapframe, ctr) == OFF_CTR * sizeof(register_t),
+	       "ctr offset");
+_Static_assert(offsetof(struct trapframe, srr0)
+	       == OFF_SRR0 * sizeof(register_t), "srr0 offset");
+#endif
+
+static struct trad_frame_cache *
+ppcfbsd_trapframe_cache (struct frame_info *this_frame, void **this_cache)
 {
-	fprintf_unfiltered(gdb_stderr, "XXX: %s\n", __func__);
-}
+  struct gdbarch *gdbarch = get_frame_arch (this_frame);
+  struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
+  enum bfd_endian byte_order = gdbarch_byte_order (gdbarch);
+  struct trad_frame_cache *cache;
+  CORE_ADDR base;
+  int i, regnum;
 
-void
-kgdb_trgt_new_objfile(struct objfile *objfile)
-{
-}
+  if (*this_cache)
+    return *this_cache;
 
-struct kgdb_frame_cache {
-	CORE_ADDR	pc;
-	CORE_ADDR	sp;
-};
+  cache = trad_frame_cache_zalloc (this_frame);
+  *this_cache = cache;
 
-static struct kgdb_frame_cache *
-kgdb_trgt_frame_cache(struct frame_info *next_frame, void **this_cache)
-{
-	char buf[MAX_REGISTER_SIZE];
-	struct kgdb_frame_cache *cache;
+  base = get_frame_register_unsigned (this_frame, gdbarch_sp_regnum (gdbarch));
+  if (tdep->wordsize == 8)
+    base += 48;
+  else
+    base += 8;
 
-	cache = *this_cache;
-	if (cache == NULL) {
-		cache = FRAME_OBSTACK_ZALLOC(struct kgdb_frame_cache);
-		*this_cache = cache;
-		cache->pc = frame_func_unwind(next_frame);
-		frame_unwind_register(next_frame, SP_REGNUM, buf);
-		cache->sp = extract_unsigned_integer(buf,
-		    register_size(current_gdbarch, SP_REGNUM));
-	}
-	return (cache);
+  for (i = 0; i < ppc_num_gprs; i++)
+    trad_frame_set_reg_addr (cache, tdep->ppc_gp0_regnum + i, base
+			     + (OFF_FIXREG + i) * tdep->wordsize);
+  trad_frame_set_reg_addr (cache, tdep->ppc_lr_regnum, base
+			   + OFF_LR * tdep->wordsize);
+  trad_frame_set_reg_addr (cache, tdep->ppc_cr_regnum, base
+			   + OFF_CR * tdep->wordsize);
+  trad_frame_set_reg_addr (cache, tdep->ppc_xer_regnum, base
+			   + OFF_XER * tdep->wordsize);
+  trad_frame_set_reg_addr (cache, tdep->ppc_ctr_regnum, base
+			   + OFF_CTR * tdep->wordsize);
+  /* SRR0?  */
+  trad_frame_set_reg_addr (cache, gdbarch_pc_regnum (gdbarch), base
+			   + OFF_SRR0 * tdep->wordsize);
+
+  /* Construct the frame ID using the function start.  */
+  trad_frame_set_id (cache, frame_id_build (base, get_frame_func (this_frame)));
+  
+  return cache;
 }
 
 static void
-kgdb_trgt_trapframe_this_id(struct frame_info *next_frame, void **this_cache,
-    struct frame_id *this_id)
+ppcfbsd_trapframe_this_id (struct frame_info *this_frame,
+			       void **this_cache, struct frame_id *this_id)
 {
-	struct kgdb_frame_cache *cache;
+  struct trad_frame_cache *cache =
+    ppcfbsd_trapframe_cache (this_frame, this_cache);
 
-	cache = kgdb_trgt_frame_cache(next_frame, this_cache);
-	*this_id = frame_id_build(cache->sp, cache->pc);
+  trad_frame_get_id (cache, this_id);
 }
 
-static void
-kgdb_trgt_trapframe_prev_register(struct frame_info *next_frame,
-    void **this_cache, int regnum, int *optimizedp, enum lval_type *lvalp,
-    CORE_ADDR *addrp, int *realnump, void *valuep)
+static struct value *
+ppcfbsd_trapframe_prev_register (struct frame_info *this_frame,
+				     void **this_cache, int regnum)
 {
-	char dummy_valuep[MAX_REGISTER_SIZE];
-	struct gdbarch_tdep *tdep;
-	struct kgdb_frame_cache *cache;
-	int ofs, regsz;
+  struct trad_frame_cache *cache =
+    ppcfbsd_trapframe_cache (this_frame, this_cache);
 
-	tdep = gdbarch_tdep(current_gdbarch);
-	regsz = register_size(current_gdbarch, regnum);
-
-	if (valuep == NULL)
-		valuep = dummy_valuep;
-	memset(valuep, 0, regsz);
-	*optimizedp = 0;
-	*addrp = 0;
-	*lvalp = not_lval;
-	*realnump = -1;
-
-	if (regnum >= tdep->ppc_gp0_regnum &&
-	    regnum <= tdep->ppc_gplast_regnum)
-		ofs = offsetof(struct trapframe,
-		    fixreg[regnum - tdep->ppc_gp0_regnum]);
-	else if (regnum == tdep->ppc_lr_regnum)
-		ofs = offsetof(struct trapframe, lr);
-	else if (regnum == tdep->ppc_cr_regnum)
-		ofs = offsetof(struct trapframe, cr);
-	else if (regnum == tdep->ppc_xer_regnum)
-		ofs = offsetof(struct trapframe, xer);
-	else if (regnum == tdep->ppc_ctr_regnum)
-		ofs = offsetof(struct trapframe, ctr);
-	else if (regnum == PC_REGNUM)
-		ofs = offsetof(struct trapframe, srr0);
-	else
-		return;
-
-	cache = kgdb_trgt_frame_cache(next_frame, this_cache);
-	*addrp = cache->sp + 8 + ofs;
-	*lvalp = lval_memory;
-	target_read_memory(*addrp, valuep, regsz);
+  return trad_frame_get_register (cache, this_frame, regnum);
 }
 
-static const struct frame_unwind kgdb_trgt_trapframe_unwind = {
-        UNKNOWN_FRAME,
-        &kgdb_trgt_trapframe_this_id,
-        &kgdb_trgt_trapframe_prev_register
+static int
+ppcfbsd_trapframe_sniffer (const struct frame_unwind *self,
+			       struct frame_info *this_frame,
+			       void **this_cache)
+{
+  CORE_ADDR pc;
+  const char *name;
+
+  pc = get_frame_func (this_frame);
+  find_pc_partial_function (pc, &name, NULL, NULL);
+  if (name && (strcmp(name, "asttrapexit") == 0
+	       || strcmp(name, "trapexit") == 0))
+    return 1;
+
+  return 0;
+}
+
+static const struct frame_unwind ppcfbsd_trapframe_unwind =
+{
+  NORMAL_FRAME,
+  default_frame_unwind_stop_reason,
+  ppcfbsd_trapframe_this_id,
+  ppcfbsd_trapframe_prev_register,
+  NULL,
+  ppcfbsd_trapframe_sniffer
 };
 
-const struct frame_unwind *
-kgdb_trgt_trapframe_sniffer(struct frame_info *next_frame)
+static void
+ppcfbsd_kernel_init_abi(struct gdbarch_info info, struct gdbarch *gdbarch)
 {
-	char *pname;
-	CORE_ADDR pc;
+  struct gdbarch_tdep *tdep = gdbarch_tdep (gdbarch);
 
-	pc = frame_pc_unwind(next_frame);
-	pname = NULL;
-	find_pc_partial_function(pc, &pname, NULL, NULL);
-	if (pname == NULL)
-		return (NULL);
-	if (strcmp(pname, "asttrapexit") == 0 ||
-	    strcmp(pname, "trapexit") == 0)
-		return (&kgdb_trgt_trapframe_unwind);
-	/* printf("%s: %llx =%s\n", __func__, pc, pname); */
-	return (NULL);
+  frame_unwind_prepend_unwinder(gdbarch, &ppcfbsd_trapframe_unwind);
+
+  set_solib_ops(gdbarch, &kld_so_ops);
+
+#ifdef __powerpc__
+  if (tdep->wordsize == sizeof(register_t))
+    {
+      fbsd_vmcore_set_supply_pcb(gdbarch, ppcfbsd_supply_pcb);
+      fbsd_vmcore_set_cpu_pcb_addr(gdbarch, ppcfbsd_cpu_pcb_addr);
+    }
+#endif
+
+  /* FreeBSD doesn't support the 128-bit `long double' from the psABI.  */
+  set_gdbarch_long_double_bit (gdbarch, 64);
+  set_gdbarch_long_double_format (gdbarch, floatformats_ieee_double);
+
+  if (tdep->wordsize == 4)
+    {
+      set_gdbarch_return_value (gdbarch, ppc_sysv_abi_broken_return_value);
+    }
+
+  if (tdep->wordsize == 8)
+    {
+      set_gdbarch_convert_from_func_ptr_addr
+	(gdbarch, ppc64_convert_from_func_ptr_addr);
+      set_gdbarch_elf_make_msymbol_special (gdbarch,
+					    ppc64_elf_make_msymbol_special);
+    }
+}
+
+void _initialize_ppc_kgdb_tdep(void);
+
+void
+_initialize_ppc_kgdb_tdep(void)
+{
+	gdbarch_register_osabi (bfd_arch_powerpc, bfd_mach_ppc,
+	    GDB_OSABI_FREEBSD_ELF_KERNEL, ppcfbsd_kernel_init_abi);
+	gdbarch_register_osabi (bfd_arch_powerpc, bfd_mach_ppc64,
+	    GDB_OSABI_FREEBSD_ELF_KERNEL, ppcfbsd_kernel_init_abi);
+
+	/* Not sure about this one. */
+	gdbarch_register_osabi (bfd_arch_rs6000, 0,
+	    GDB_OSABI_FREEBSD_ELF_KERNEL, ppcfbsd_kernel_init_abi);
 }
