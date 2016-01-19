cf71cb5f3bc8d2109b067f10ab1586e01f499405
John Baldwin <jhb@FreeBSD.org>
2016-01-19 08:49:04 -0800

    Sync with uptsream version.
    Add a new gdbarch method for fetching thread names from cores.
    Drop unused gdbarch from core generation.

--- gdb/fbsd-tdep.c
+++ gdb/fbsd-tdep.c
@@ -34,44 +34,62 @@ static char *
 fbsd_core_pid_to_str (struct gdbarch *gdbarch, ptid_t ptid)
 {
   static char buf[80];
-  struct bfd_section *section;
-  bfd_size_type size;
-  char sectionstr[32];
 
   if (ptid_get_lwp (ptid) != 0)
     {
-      xsnprintf (sectionstr, sizeof sectionstr, ".thrmisc/%ld",
-		ptid_get_lwp (ptid));
-      section = bfd_get_section_by_name (core_bfd, sectionstr);
-      if (section != NULL && bfd_section_size (core_bfd, section) > 0)
-	{
-	  char *name;
-
-	  size = bfd_section_size (core_bfd, section);
-	  name = alloca (size + 1);
-	  if (bfd_get_section_contents (core_bfd, section, name, (file_ptr) 0,
-					size)
-	      && name[0] != '\0')
-	    {
-	      name[size] = '\0';
-
-	      /* Note that each thread will report the process command
-		 as its thread name instead of an empty name if a name
-		 has not been set explicitly.  Return a NULL name in
-		 that case.  */
-	      if (strcmp (name, elf_tdata (core_bfd)->core->program) != 0)
-		{
-		  xsnprintf (buf, sizeof buf, "LWP %ld \"%s\"",
-			    ptid_get_lwp (ptid), name);
-		  return buf;
-		}
-	    }
-	}
       xsnprintf (buf, sizeof buf, "LWP %ld", ptid_get_lwp (ptid));
       return buf;
     }
 
   return normal_pid_to_str (ptid);
+}
+
+/* Extract the name assigned to a thread from a core.  Returns the
+   string in a static buffer.  */
+
+static const char *
+fbsd_core_thread_name (struct gdbarch *gdbarch, struct thread_info *thr)
+{
+  static char buf[80];
+  struct bfd_section *section;
+  bfd_size_type size;
+  char sectionstr[32];
+
+  if (ptid_get_lwp (thr->ptid) != 0)
+    {
+      /* FreeBSD includes a NT_FREEBSD_THRMISC note for each thread
+	 whose contents are defined by a "struct thrmisc" declared in
+	 <sys/procfs.h> on FreeBSD.  The per-thread name is stored as
+	 a null-terminated string as the first member of the
+	 structure.  Rather than define the full structure here, just
+	 extract the null-terminated name from the start of the
+	 note.  */
+      xsnprintf (sectionstr, sizeof sectionstr, ".thrmisc/%ld",
+		ptid_get_lwp (thr->ptid));
+      section = bfd_get_section_by_name (core_bfd, sectionstr);
+      if (section != NULL && bfd_section_size (core_bfd, section) > 0)
+	{
+	  /* Truncate the name if it is longer than "buf".  */
+	  size = bfd_section_size (core_bfd, section);
+	  if (size > sizeof buf - 1)
+	    size = sizeof buf - 1;
+	  if (bfd_get_section_contents (core_bfd, section, buf, (file_ptr) 0,
+					size)
+	      && buf[0] != '\0')
+	    {
+	      buf[size] = '\0';
+
+	      /* Note that each thread will report the process command
+		 as its thread name instead of an empty name if a name
+		 has not been set explicitly.  Return a NULL name in
+		 that case.  */
+	      if (strcmp (buf, elf_tdata (core_bfd)->core->program) != 0)
+		return buf;
+	    }
+	}
+    }
+
+  return NULL;
 }
 
 static int
@@ -90,7 +108,6 @@ find_signalled_thread (struct thread_info *info, void *data)
 
 struct fbsd_collect_regset_section_cb_data
 {
-  struct gdbarch *gdbarch;
   const struct regcache *regcache;
   bfd *obfd;
   char *note_data;
@@ -106,7 +123,8 @@ fbsd_collect_regset_section_cb (const char *sect_name, int size,
 				const char *human_name, void *cb_data)
 {
   char *buf;
-  struct fbsd_collect_regset_section_cb_data *data = cb_data;
+  struct fbsd_collect_regset_section_cb_data *data
+    = (struct fbsd_collect_regset_section_cb_data *) cb_data;
 
   if (data->abort_iteration)
     return;
@@ -143,7 +161,6 @@ fbsd_collect_thread_registers (const struct regcache *regcache,
   struct gdbarch *gdbarch = get_regcache_arch (regcache);
   struct fbsd_collect_regset_section_cb_data data;
 
-  data.gdbarch = gdbarch;
   data.regcache = regcache;
   data.obfd = obfd;
   data.note_data = note_data;
@@ -272,5 +289,6 @@ void
 fbsd_init_abi (struct gdbarch_info info, struct gdbarch *gdbarch)
 {
   set_gdbarch_core_pid_to_str (gdbarch, fbsd_core_pid_to_str);
+  set_gdbarch_core_thread_name (gdbarch, fbsd_core_thread_name);
   set_gdbarch_make_corefile_notes (gdbarch, fbsd_make_corefile_notes);
 }
