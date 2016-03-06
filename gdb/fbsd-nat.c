c11a3508d3de4ff0791d7f092b5a906ed64e916c
John Baldwin <jhb@FreeBSD.org>
2016-03-06 00:21:14 -0800

    Rather than trying to handle LWPs arriving out of the blue, scan for them explicitly before trying to suspend all but 1 thread in a process.  This might still result in some excitement if a single-step creates a new thread which manages to hit a breakpoint and report it before the "real" thread completes its single step.

--- gdb/fbsd-nat.c
+++ gdb/fbsd-nat.c
@@ -568,6 +568,14 @@ fbsd_resume (struct target_ops *ops,
 #endif
   if (ptid_lwp_p (ptid))
     {
+#ifndef PT_LWP_EVENTS
+      /* When LWP events are not supported, a new thread might already be
+	 running that has not yet reported an event when GDB wishes to
+	 only run a single thread.  Force an update of the thread list
+	 to ensure that any such threads are suspended before the process
+	 is resumed.  */
+      fbsd_add_threads (ptid_get_pid (ptid));
+#endif
       /* If ptid is a specific LWP, suspend all other LWPs in the process.  */
       iterate_over_threads (resume_one_thread_cb, &ptid);
     }
