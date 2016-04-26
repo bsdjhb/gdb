ea5f30cdd6a5fa8b6d10764cb91e16da108417e5
John Baldwin <jhb@FreeBSD.org>
2016-04-25 19:00:02 -0700

    Import patch-gdb-python-python-config.py

--- gdb/python/python-config.py
+++ gdb/python/python-config.py
@@ -59,6 +59,8 @@ for opt in opt_flags:
 
     elif opt in ('--libs', '--ldflags'):
         libs = []
+        if getvar('LDFLAGS') is not None:
+            libs.extend(getvar('LDFLAGS').split())
         if getvar('LIBS') is not None:
             libs.extend(getvar('LIBS').split())
         if getvar('SYSLIBS') is not None:
