52e4cb3f042509c5b4f370f758c0a57c44ec4080
John Baldwin <jhb@FreeBSD.org>
2016-04-25 18:57:26 -0700

    Import patch-gdb-i386fbsd-nat.c.

--- gdb/i386-fbsd-nat.c
+++ gdb/i386-fbsd-nat.c
@@ -163,7 +163,6 @@ _initialize_i386fbsd_nat (void)
   t->to_read_description = i386fbsd_read_description;
 #endif
 
-  t->to_resume = i386fbsd_resume;
   fbsd_nat_add_target (t);
 
   /* Support debugging kernel virtual memory images.  */
