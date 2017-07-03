4c6ed9177fbe0c3f2fed69fe70a8d36e442644a3
John Baldwin <jhb@FreeBSD.org>
2017-07-02 19:26:05 -0700

    Import patch-fixes.

--- gdb/compile/compile-loc2c.c
+++ gdb/compile/compile-loc2c.c
@@ -669,6 +669,7 @@ do_compile_dwarf_expr_to_c (int indent, string_file &stream,
       uint64_t uoffset, reg;
       int64_t offset;
 
+      uoffset = 0;
       print_spaces (indent - 2, &stream);
       if (info[op_ptr - base].label)
 	{
