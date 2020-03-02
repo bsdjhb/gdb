55e44936b770da4391724c7d614a01e368f1f9da
John Baldwin <jhb@FreeBSD.org>
2020-03-02 12:09:04 -0800

    Add gdbarch methods for converting between pointers and integers.
    These are adapted from CHERI MIPS.  However, CHERI-RISC-V stores the
    virtual address in the first XLEN bytes.

--- gdb/riscv-tdep.c
+++ gdb/riscv-tdep.c
@@ -472,6 +472,14 @@ riscv_is_fp_regno_p (int regno)
 	  && regno <= RISCV_LAST_FP_REGNUM);
 }
 
+/* Return true if the target for GDBARCH has capability registers.  */
+
+static bool
+riscv_has_cheri (struct gdbarch *gdbarch)
+{
+  return (riscv_isa_clen (gdbarch) > 0);
+}
+
 /* Implement the breakpoint_kind_from_pc gdbarch method.  */
 
 static int
@@ -3085,6 +3093,110 @@ riscv_dwarf_reg_to_regnum (struct gdbarch *gdbarch, int reg)
   return -1;
 }
 
+static CORE_ADDR
+riscv_cheri_pointer_to_address (struct gdbarch *gdbarch, struct type *type,
+				const gdb_byte *buf)
+{
+  enum bfd_endian byte_order = gdbarch_byte_order (gdbarch);
+
+  if (type->length <= riscv_isa_xlen (gdbarch))
+    return signed_pointer_to_address (gdbarch, type, buf);
+  else
+    return extract_signed_integer (buf, riscv_isa_xlen (gdbarch), byte_order);
+}
+
+/* XXX: This does not generate a valid capability.  However, a
+   round-trip that converts an address to a pointer back to an address
+   will work with this implementation. */
+
+static void
+riscv_cheri_address_to_pointer (struct gdbarch *gdbarch, struct type *type,
+				gdb_byte *buf, CORE_ADDR addr)
+{
+  enum bfd_endian byte_order = gdbarch_byte_order (gdbarch);
+
+  if (type->length <= riscv_isa_xlen (gdbarch))
+    address_to_signed_pointer (gdbarch, type, buf, addr);
+  else
+    {
+      memset (buf, 0, type->length);
+      store_signed_integer (buf, riscv_isa_xlen (gdbarch), byte_order, addr);
+    }
+}
+
+static CORE_ADDR
+riscv_cheri_integer_to_address (struct gdbarch *gdbarch,
+				struct type *type, const gdb_byte *buf)
+{
+  return riscv_cheri_pointer_to_address (gdbarch, type, buf);
+}
+
+static struct value *
+riscv_cheri_cast_integer_to_pointer (struct gdbarch *gdbarch,
+				     struct type *type, struct value *arg2)
+{
+  if (TYPE_LENGTH (type) > riscv_isa_xlen (gdbarch))
+    {
+      struct type *type2 = check_typedef (value_type (arg2));
+
+      /* Handle uintcap_t and intcap_t.  */
+      if (TYPE_LENGTH (type) == TYPE_LENGTH (type2))
+	{
+	  struct value *v = value_copy (arg2);
+	  deprecated_set_value_type (v, type);
+	  set_value_enclosing_type (v, type);
+	  set_value_pointed_to_offset (v, 0);
+	  return v;
+	}
+
+      if (TYPE_LENGTH (type2) <= riscv_isa_xlen (gdbarch))
+	{
+	  /* Construct a capability using the integer value as the cursor.  */
+	  struct value *v = allocate_value (type);
+	  store_signed_integer (value_contents_writeable (v),
+				riscv_isa_xlen (gdbarch),
+				gdbarch_byte_order (gdbarch),
+				value_as_long (arg2));
+	  return v;
+	}
+    }
+  return NULL;
+}
+
+static struct value *
+riscv_cheri_cast_pointer_to_integer (struct gdbarch *gdbarch,
+				     struct type *type, struct value *arg2)
+{
+  struct type *type2 = check_typedef (value_type (arg2));
+
+  if (TYPE_LENGTH (type2) > riscv_isa_xlen (gdbarch))
+    {
+      /* Handle uintcap_t and intcap_t.  */
+      if (TYPE_LENGTH (type) == TYPE_LENGTH (type2))
+	{
+	  struct value *v = value_copy (arg2);
+	  deprecated_set_value_type (v, type);
+	  set_value_enclosing_type (v, type);
+	  set_value_pointed_to_offset (v, 0);
+	  return v;
+	}
+
+      if (TYPE_LENGTH (type) <= riscv_isa_xlen (gdbarch))
+	{
+	  /* Read the cursor and return that as an integer value.
+	     This will always return the address instead of the
+	     offset.  */
+	  LONGEST longest;
+
+	  longest = extract_signed_integer (value_contents (arg2),
+					    riscv_isa_xlen (gdbarch),
+					    gdbarch_byte_order (gdbarch));
+	  return value_from_longest (type, longest);
+	}
+    }
+  return NULL;
+}
+
 /* Initialize the current architecture based on INFO.  If possible,
    re-use an architecture from ARCHES, which is a list of
    architectures already created during this debugging session.
@@ -3300,6 +3412,18 @@ riscv_gdbarch_init (struct gdbarch_info info,
 
   /* Register architecture.  */
   riscv_add_reggroups (gdbarch);
+
+  /* Settings for CHERI processors.  */
+  if (riscv_has_cheri (gdbarch))
+    {
+      set_gdbarch_pointer_to_address (gdbarch, riscv_cheri_pointer_to_address);
+      set_gdbarch_address_to_pointer (gdbarch, riscv_cheri_address_to_pointer);
+      set_gdbarch_integer_to_address (gdbarch, riscv_cheri_integer_to_address);
+      set_gdbarch_cast_integer_to_pointer (gdbarch,
+					   riscv_cheri_cast_integer_to_pointer);
+      set_gdbarch_cast_pointer_to_integer (gdbarch,
+					   riscv_cheri_cast_pointer_to_integer);
+    }
 
   /* Internal <-> external register number maps.  */
   set_gdbarch_dwarf2_reg_to_regnum (gdbarch, riscv_dwarf_reg_to_regnum);
