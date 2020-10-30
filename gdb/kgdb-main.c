ffd270644ed49d1b7d9e6ea0d1bf9fc6f2ef2141
John Baldwin <jhb@FreeBSD.org>
2020-10-30 14:21:24 -0700

    Eliminate most of the compile warnings from kgdb.
    - Add prototypes for _initialize_* to match upstream.
    - Stop using __FBSDID() which requires <sys/cdefs.h>.

--- gdb/kgdb-main.c
+++ gdb/kgdb-main.c
@@ -22,10 +22,9 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $FreeBSD: head/gnu/usr.bin/gdb/kgdb/main.c 260027 2013-12-28 23:31:22Z marcel $
  */
-
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/gnu/usr.bin/gdb/kgdb/main.c 260027 2013-12-28 23:31:22Z marcel $");
 
 #include <sys/param.h>
 #include <sys/stat.h>
