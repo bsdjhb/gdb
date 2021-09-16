fdf1579c1900399f1b8b2e4dd3421aa6ea62f1e7
John Baldwin <jhb@FreeBSD.org>
2021-09-16 14:06:44 -0700

    Drop $FreeBSD$.

--- gdb/mipsfbsd-kern.c
+++ gdb/mipsfbsd-kern.c
@@ -23,10 +23,6 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * from: src/gnu/usr.bin/gdb/kgdb/trgt_alpha.c,v 1.2.2.1 2005/09/15 05:32:10 marcel
- *
- * $FreeBSD: head/gnu/usr.bin/gdb/kgdb/trgt_mips.c 249878 2013-04-25 04:53:01Z imp $
  */
 
 #include "defs.h"
